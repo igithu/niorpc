48e36602fbda7b4ddaadc427ab40d3bbe71ca8ec
alisen <asy5178@163.com>
2017-01-02 00:02:25 +0800

    add nio rpc server

---
+++ niorpc-server/src/main/java/com/igithu/niorpc/Server.java
@@ -0,0 +1,44 @@
+/***************************************************************************
+ *
+ * Copyright (c) 2017 aishuyu, Inc. All Rights Reserved
+ *
+ **************************************************************************/
+
+
+
+/**
+ * @file Server.java
+ * @author aishuyu(asy5178@163.com)
+ * @date 2017/01/01 23:35:27
+ * @brief
+ *
+ **/
+
+
+import java.util.concurrent.Executors;
+import com.googlecode.protobuf.socketrpc.RpcServer;
+import com.googlecode.protobuf.socketrpc.ServerRpcConnectionFactory;
+import com.googlecode.protobuf.socketrpc.SocketRpcConnectionFactories;
+
+public class Server {
+    private int port;
+    private int threadPoolSize;
+
+    public void Start() {
+    }
+
+    public static void main(String[] args) {
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
